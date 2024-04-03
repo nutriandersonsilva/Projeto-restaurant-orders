e19f65ad295e769ac75686360f057fb5d6465ee5
Anderson Silva <nutriandersonsilva@gmail.com>
2024-04-03 19:20:34 -0300

    atividade 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("farinha")
+    ingredient2 = Ingredient("farinha")
+    ingredient3 = Ingredient("ovo")
+
+    assert ingredient1.name == "farinha"
+    assert ingredient1.restrictions == {Restriction.GLUTEN}
+
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    assert ingredient1.__hash__() == ingredient2.__hash__()
+    assert ingredient1.__hash__() != ingredient3.__hash__()
+
+    assert ingredient1.__repr__() == "Ingredient('farinha')"
+    assert ingredient1.__repr__() == ingredient2.__repr__()
