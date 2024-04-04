328d41913dc4eb9754ab39486c30fa92fd8950ba
Anderson Silva <nutriandersonsilva@gmail.com>
2024-04-03 21:10:45 -0300

    atividade 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
-# Req 3
+import csv
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = []
+
+        with open(source_path, "r") as file:
+            file_reader = csv.reader(file, delimiter=",")
+            header, *data = file_reader
+
+            for name, price, ingredient, recipe_amount in data:
+                price = float(price)
+                recipe_amount = int(recipe_amount)
+
+                # Verifica se o prato já existe na lista, se não, cria um novo
+                dish = next((d for d in self.dishes if d.name == name), None)
+                if dish is None:
+                    dish = Dish(name, price)
+                    self.dishes.append(dish)
+
+                # Adiciona o ingrediente ao prato
+                new_ingr = Ingredient(ingredient)
+                dish.add_ingredient_dependency(new_ingr, recipe_amount)
